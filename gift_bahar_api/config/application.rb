01f877dffb2a60e228e22893a39938c468a134af
Bikash Khanal <iambkhanal@gmail.com>
2024-03-06 19:48:17 +0545

    replaced mvc template with api template

--- gift_bahar_api/config/application.rb
+++ gift_bahar_api/config/application.rb
@@ -6,11 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
-if ['development', 'test'].include? ENV['RAILS_ENV']
-  Dotenv::Railtie.load
-end
-
 module GiftBaharApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -28,5 +23,10 @@ module GiftBaharApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
